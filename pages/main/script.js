f523dc5f1e416bff68b3a2ea8120aba19086f651
Anastasia Shapetko <shapetkonnn@gmail.com>
2022-12-14 19:03:09 +0500

    fix: fix errors in the testimonial block

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,27 +1,28 @@
 // Burger-menu
-const tabletSize = 1000;
-const mobileSize = 640;
-let screenType;
-const changeSlider = new Event('changeSlider');
 
-// burger menu
-const burgerMenu = document.querySelector('.burger_icon');
-const menuBody = document.querySelector('.menu_body');
-const headerLogo = document.querySelector('header').querySelector('.logo');
+// const tabletSize = 1000;
+// const mobileSize = 640;
+// let screenType;
+// const changeSlider = new Event('changeSlider');
 
-// open/close burger menu by click on burger menu icon
-burgerMenu.addEventListener('click', function (e) {
-  burgerMenu.classList.toggle('_active');
-  document.body.classList.toggle('_lock');
-  menuBody.classList.toggle('_active');
-  headerLogo.classList.toggle('burger_logo');
-});
-// close opened burger menu by click on free area burger menu
-menuBody.addEventListener('click', (e) => {
-  if (menuBody.classList.contains('_active') && e.target === menuBody) {
-    burgerMenu.classList.toggle('_active');
-    document.body.classList.toggle('_lock');
-    menuBody.classList.toggle('_active');
-    headerLogo.classList.toggle('burger_logo');
-  }
-});
+// // burger menu
+// const burgerMenu = document.querySelector('.burger_icon');
+// const menuBody = document.querySelector('.menu_body');
+// const headerLogo = document.querySelector('header').querySelector('.logo');
+
+// // open/close
+// burgerMenu.addEventListener('click', function (e) {
+//   burgerMenu.classList.toggle('_active');
+//   document.body.classList.toggle('_lock');
+//   menuBody.classList.toggle('_active');
+//   headerLogo.classList.toggle('burger_logo');
+// });
+// // close
+// menuBody.addEventListener('click', (e) => {
+//   if (menuBody.classList.contains('_active') && e.target === menuBody) {
+//     burgerMenu.classList.toggle('_active');
+//     document.body.classList.toggle('_lock');
+//     menuBody.classList.toggle('_active');
+//     headerLogo.classList.toggle('burger_logo');
+//   }
+// });
